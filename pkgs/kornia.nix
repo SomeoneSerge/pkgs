61620f5a93618c01846f61006fa91bc0edb29784
Someone Serge <sergei.kozlukov@aalto.fi>
2022-03-05 23:50:39 +0200

    kornia: enable all tests

--- pkgs/kornia.nix
+++ pkgs/kornia.nix
@@ -18,7 +18,6 @@ buildPythonPackage rec {
     sha256 = "sha256-7CpONUpuZX5FkRkWBj+VH3rWhbCmyNfYc+IzaaiLJ1w=";
   };
   checkInputs = [ pytest-runner pytestCheckHook scipy ];
-  disabledTestPaths = [ "test/test_contrib.py" "test/x" "test/feature" "test/geometry" "test/tracking" ];
   propagatedBuildInputs = [ pytorch ];
 
   meta = {
