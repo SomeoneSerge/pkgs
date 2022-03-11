c5671fdde7393411fb08c6553f9e68b296c27279
Someone Serge <sergei.kozlukov@aalto.fi>
2022-03-11 19:52:14 +0200

    ci: hercules-ci jobs

--- ci.nix
+++ ci.nix
@@ -1,53 +1,38 @@
-# This file provides all the buildable and cacheable packages and
-# package outputs in your package set. These are what gets built by CI,
-# so if you correctly mark packages as
-#
-# - broken (using `meta.broken`),
-# - unfree (using `meta.license.free`), and
-# - locally built (using `preferLocalBuild`)
-#
-# then your CI will be able to build and cache only those packages for
-# which this is possible.
-
-{ pkgs ? import <nixpkgs> { } }:
-
-with builtins;
+# Hercules CI jobs
 let
-  isReserved = n: n == "lib" || n == "overlays" || n == "modules";
-  isDerivation = p: isAttrs p && p ? type && p.type == "derivation";
-  isBuildable = p: !(p.meta.broken or false) && p.meta.license.free or true;
-  isCacheable = p: !(p.preferLocalBuild or false);
-  shouldRecurseForDerivations = p: isAttrs p && p.recurseForDerivations or false;
+  flake = builtins.getFlake (builtins.toString ./.);
+  inherit (flake) inputs;
+  inherit (flake.inputs.nixpkgs-release) lib;
 
-  nameValuePair = n: v: { name = n; value = v; };
-
-  concatMap = builtins.concatMap or (f: xs: concatLists (map f xs));
-
-  flattenPkgs = s:
+  branches = [ "release" "unstable" ];
+  systems = [ "i686-linux" "x86_64-linux" ];
+  configs = [
+    { allowUnfree = false; }
+    { allowUnfree = true; cudaSupport = true; }
+  ];
+  matrix = lib.cartesianProductOfSets {
+    branch = branches;
+    system = systems;
+    config = configs;
+  };
+  # E.g. [] or ["unfree" "cuda"]
+  cfgToStrings = c:
+    lib.optional c.allowUnfree or false "unfree"
+    ++ lib.optional c.cudaSupport or false "cuda";
+  # E.g. "release-x86_64-linux-unfree-cuda"
+  mkName = { branch, system, config }:
+    let strings = [
+      branch
+      system
+    ] ++ cfgToStrings config;
+    in lib.concatStringsSep "-" strings;
+  importOuts = { branch, system, config }@combination:
     let
-      f = p:
-        if shouldRecurseForDerivations p then flattenPkgs p
-        else if isDerivation p then [ p ]
-        else [ ];
+      # pkgs = inputs."nixpkgs-${branch}".legacyPackages."${system}";
+      pkgs = import inputs."nixpkgs-${branch}" { inherit system config; };
+      outs = lib.recurseIntoAttrs (pkgs.callPackage ./default.nix { });
     in
-    concatMap f (attrValues s);
-
-  outputsOf = p: map (o: p.${o}) p.outputs;
-
-  nurAttrs = import ./default.nix { inherit pkgs; };
-
-  nurPkgs =
-    flattenPkgs
-      (listToAttrs
-        (map (n: nameValuePair n nurAttrs.${n})
-          (filter (n: !isReserved n)
-            (attrNames nurAttrs))));
-
+    lib.nameValuePair (mkName combination) outs;
+  outsAll = lib.listToAttrs (builtins.map importOuts matrix);
 in
-rec {
-  buildPkgs = filter isBuildable nurPkgs;
-  cachePkgs = filter isCacheable buildPkgs;
-
-  buildOutputs = concatMap outputsOf buildPkgs;
-  cacheOutputs = concatMap outputsOf cachePkgs;
-}
+outsAll
--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }:
+  inputs.nixpkgs-release.url = "github:NixOS/nixpkgs/nixos-21.11";
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  outputs = { self, nixpkgs-release, nixpkgs-unstable }:
     let
-      inherit (nixpkgs) lib;
+      inherit (nixpkgs-release) lib;
       systems = [
         "x86_64-linux"
         "i686-linux"
@@ -18,13 +19,13 @@
         let
           filters = [
             (name: supportsPlatform system)
-            (name: package: !package.meta.broken)
+            (name: package: !(package.meta.broken or true))
           ];
           f = name: package: builtins.all (f: f name package) filters;
         in
         lib.filterAttrs f packages;
       importPkgs = system: import ./default.nix {
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = nixpkgs-release.legacyPackages.${system};
       };
       allAttrs = forAllSystems (system: importPkgs system);
       allPackages = lib.mapAttrs (system: packages: builtins.removeAttrs packages [ "lib" "overlays" "modules" ]) allAttrs;
