b37c6e91ab9119c69096244484fc3f864477dfe3
Someone Serge <sergei.kozlukov@aalto.fi>
2022-03-12 01:58:53 +0200

    hercules: reuse filters from nur-ci, change naming

--- ci.nix
+++ ci.nix
@@ -6,33 +6,52 @@ let
 
   branches = [ "release" "unstable" ];
   systems = [ "i686-linux" "x86_64-linux" ];
-  configs = [
-    { allowUnfree = false; }
-    { allowUnfree = true; cudaSupport = true; }
-  ];
+  args = {
+    vanilla = { };
+    cuda = {
+      config.allowUnfree = true;
+      config.cudaSupport = true;
+      overlays = [
+        (final: prev:
+          let forceLocal = a: {
+            preferLocalBuild = true;
+            allowSubstitutes = false;
+          };
+          in
+          {
+            cudatoolkit = prev.cudatoolkit.overrideAttrs forceLocal;
+            cudatoolkit_11 = prev.cudatoolkit_11.overrideAttrs forceLocal;
+          })
+      ];
+    };
+  };
+
   matrix = lib.cartesianProductOfSets {
     branch = branches;
     system = systems;
-    config = configs;
+    argsName = lib.attrNames args;
   };
-  # E.g. [] or ["unfree" "cuda"]
-  cfgToStrings = c:
-    lib.optional c.allowUnfree or false "unfree"
-    ++ lib.optional c.cudaSupport or false "cuda";
-  # E.g. "release-x86_64-linux-unfree-cuda"
-  mkName = { branch, system, config }:
-    let strings = [
-      branch
-      system
-    ] ++ cfgToStrings config;
-    in lib.concatStringsSep "-" strings;
-  importOuts = { branch, system, config }@combination:
+
+  mkName = { branch, system, argsName }:
+    let
+      args = args.${argsName};
+      strings = [
+        inputs."nixpkgs-${branch}".rev
+        branch
+        argsName
+        system
+      ];
+    in
+    lib.concatStringsSep "." strings;
+  importOuts = { branch, system, argsName }@combination:
     let
       # pkgs = inputs."nixpkgs-${branch}".legacyPackages."${system}";
-      pkgs = import inputs."nixpkgs-${branch}" { inherit system config; };
-      outs = lib.recurseIntoAttrs (pkgs.callPackage ./default.nix { });
+      pkgs = import inputs."nixpkgs-${branch}" (args // { inherit system; });
+      outs = (pkgs.callPackage ./nur-ci.nix { }).cachePkgs;
+      outs' = builtins.listToAttrs (builtins.map (out: lib.nameValuePair out.name out) outs);
     in
-    lib.nameValuePair (mkName combination) outs;
+    lib.nameValuePair (mkName combination) (lib.recurseIntoAttrs outs');
+
   outsAll = lib.listToAttrs (builtins.map importOuts matrix);
 in
 outsAll
