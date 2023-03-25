2f98f6459ba4f3ebad66dff036e764457227c411
Someone Serge <sergei.kozlukov@aalto.fi>
2023-03-25 15:58:28 +0200

    with-cuda.nix: build against torch 2.0.0 PR

--- with-cuda.nix
+++ with-cuda.nix
@@ -1,5 +1,5 @@
 let
-  nixpkgs = builtins.getFlake github:SomeoneSerge/nixpkgs/hotfix-nvcc-gcc-incompatibility;
+  nixpkgs = builtins.getFlake github:SomeoneSerge/nixpkgs/torch20;
   pkgs = import nixpkgs {
     config.allowUnfree = true;
     config.cudaSupport = true;
