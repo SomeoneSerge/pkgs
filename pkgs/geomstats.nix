928797905f437f987138282c44f4320efe236d73
Someone Serge <sergei.kozlukov@aalto.fi>
2023-08-23 21:03:15 +0300

    geomstats: 2.5.0 -> 2.6.0

--- pkgs/geomstats.nix
+++ pkgs/geomstats.nix
@@ -13,7 +13,7 @@
 
 let
   pname = "geomstats";
-  version = "2.5.0";
+  version = "2.6.0";
 in
 buildPythonPackage {
   inherit pname version;
@@ -23,7 +23,7 @@ buildPythonPackage {
     owner = "geomstats";
     repo = pname;
     rev = "${version}";
-    hash = "sha256-3FP4pgLMjd1qHcWSkwV8bG+tlxMU3LwKCPfgqOjJfpg=";
+    hash = "sha256-A/NU5kEWhVL3FA3WAGVeMGrPuqbB6Cw5cTbzMzleHjM=";
   };
 
   nativeBuildInputs = [
