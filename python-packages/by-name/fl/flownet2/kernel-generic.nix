334bc6ce2fd5a9d63eb9aa4a9f108432897a76b5
Someone Serge <sergei.kozlukov@aalto.fi>
2023-09-26 04:58:37 +0300

    flownet2: init at unstable

---
+++ python-packages/by-name/fl/flownet2/kernel-generic.nix
@@ -0,0 +1,53 @@
+{ buildPackages
+, buildPythonPackage
+, cudaPackages
+, description
+, fetchFromGitHub
+, lib
+, pname
+, pybind11
+, src
+, stdenv
+, symlinkJoin
+, torch
+, version
+, which
+}:
+
+buildPythonPackage rec {
+  inherit pname src version;
+  postPatch = ''
+    sed -i '/cxx_args =/{ x; a cxx_args = [ "-std=c++14" ]
+      }' setup.py
+  '';
+
+  CUDA_HOME = symlinkJoin {
+    name = "cuda-unsplit";
+    paths = [
+      buildPackages.cudaPackages.cuda_nvcc
+      cudaPackages.cuda_cccl
+      cudaPackages.cuda_cudart
+      cudaPackages.libcublas
+      cudaPackages.libcusolver
+      cudaPackages.libcusparse
+    ];
+  };
+  nativeBuildInputs = [
+    cudaPackages.backendStdenv.cc
+    which
+  ];
+  buildInputs = [
+  ];
+  propagatedBuildInputs = [
+    pybind11
+    torch
+  ];
+
+  meta = with lib; {
+    inherit description;
+    homepage = "https://github.com/NVIDIA/flownet2-pytorch/tree/master/networks/resample2d_package";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ SomeoneSerge ];
+    platforms = platforms.all;
+  };
+}
