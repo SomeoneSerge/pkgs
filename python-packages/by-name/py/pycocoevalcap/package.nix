97bc3ba5882272fb6cab935b6b7d8567eb057954
Someone Serge <sergei.kozlukov@aalto.fi>
2023-11-17 02:14:23 +0000

    stable-diffusion-webui: init at BROKEN

---
+++ python-packages/by-name/py/pycocoevalcap/package.nix
@@ -0,0 +1,40 @@
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, setuptools
+, wheel
+, numpy
+}:
+
+buildPythonPackage rec {
+  pname = "pycocoevalcap";
+  version = "1.2";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "salaniz";
+    repo = "pycocoevalcap";
+    rev = "v${version}";
+    hash = "sha256-LcMcxRF8IVu7t05GXVXKzsFBxdP+LllfuV3vCGNdYvk=";
+  };
+
+  nativeBuildInputs = [
+    setuptools
+    wheel
+  ];
+
+  propagatedBuildInputs = [
+    numpy
+  ];
+
+  pythonImportsCheck = [
+    "pycocoevalcap.eval"
+  ];
+
+  meta = with lib; {
+    description = "Python 3 support for the MS COCO caption evaluation tools";
+    homepage = "https://github.com/salaniz/pycocoevalcap";
+    license = licenses.bsd2WithViews;
+    maintainers = with maintainers; [ ];
+  };
+}
