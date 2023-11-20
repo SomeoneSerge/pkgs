fe7e5a508ac6a6d444b39cb31f58ebdf06420407
Someone Serge <sergei.kozlukov@aalto.fi>
2023-11-20 14:59:26 +0000

    fixup! default.nix: unbreak NUR

--- default.nix
+++ default.nix
@@ -3,7 +3,10 @@
 
 let
   flake = import ./compat.nix { src = ./.; };
-  final = pkgs.extend flake.outputs.overlay;
+
+  # Ignoring the NUR's nixpkgs revision:
+  # final = pkgs.extend flake.outputs.overlay;
+  final = flake.outputs.legacyPackages.${pkgs.system}.pkgs;
 in
 final.some-pkgs // {
   inherit (final) some-pkgs some-datasets some-util;
