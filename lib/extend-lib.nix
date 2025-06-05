2dc92d835bbf5151e320a4d4d727204cac67abbb
SomeoneSerge <else@someonex.net>
2025-06-05 16:42:08 +0000

    lib: update maintainers, change priority

--- lib/extend-lib.nix
+++ lib/extend-lib.nix
@@ -1,48 +1,55 @@
 { inputs, oldLib }:
 let
-  diff =
-    {
-      maintainers.SomeoneSerge = {
-        email = "sergei.kozlukov@aalto.fi";
-        matrix = "@ss:someonex.net";
+  diff = {
+    maintainers.SomeoneSerge =
+      oldLib.maintainers.SomeoneSerge or {
+        email = "else+nixpkgs@someonex.net";
         github = "SomeoneSerge";
         githubId = 9720532;
-        name = "Sergei K";
+        name = "nobody";
       };
 
-      readByName = import ../read-by-name.nix { lib = oldLib; };
+    readByName = import ../read-by-name.nix { lib = oldLib; };
 
-      autocallByName = ps: baseDirectory:
-        let
-          files = lib.readByName baseDirectory;
-          packages = oldLib.mapAttrs
-            (name: { directory, kind, path }:
-              let
-                evaluatedModules = lib.evalModules {
-                  specialArgs = {
-                    inherit (inputs) dream2nix;
-                    packageSets.nixpkgs = ps;
-                  };
-                  modules = [
-                    path
-                    {
-                      paths.projectRoot = baseDirectory;
-                      paths.projectRootFile = "flake.nix";
-                      paths.package = directory;
-                      paths.lockFile = "lock.json";
-                    }
-                  ];
-                };
-                package = ps.callPackage path { };
-              in
-              if kind == "package" then package
-              else if kind == "dream2nix" then evaluatedModules.config.public
-              else throw "autocallByName: unknown kind ${kind}"
-            )
-            files;
-        in
-        packages;
-    };
+    autocallByName =
+      ps: baseDirectory:
+      let
+        files = lib.readByName baseDirectory;
+        packages = oldLib.mapAttrs (
+          name:
+          {
+            directory,
+            kind,
+            path,
+          }:
+          let
+            evaluatedModules = lib.evalModules {
+              specialArgs = {
+                inherit (inputs) dream2nix;
+                packageSets.nixpkgs = ps;
+              };
+              modules = [
+                path
+                {
+                  paths.projectRoot = baseDirectory;
+                  paths.projectRootFile = "flake.nix";
+                  paths.package = directory;
+                  paths.lockFile = "lock.json";
+                }
+              ];
+            };
+            package = ps.callPackage path { };
+          in
+          if kind == "package" then
+            package
+          else if kind == "dream2nix" then
+            evaluatedModules.config.public
+          else
+            throw "autocallByName: unknown kind ${kind}"
+        ) files;
+      in
+      packages;
+  };
   lib = oldLib.recursiveUpdate oldLib diff;
 in
 {
